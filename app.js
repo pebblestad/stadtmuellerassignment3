0dcf3810e80f49a9a3b4079af450a010ba9ac656
pebblestad <stadtmueller.johnathan@yahoo.com>
2019-12-04 21:07:44 +0000

    more app.js work

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ var bodyParser = require('body-parser');
 const nodeFetch = require('node-fetch');
 
 const express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, "html"));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    let url = 'https://xkcd.com/info.0.json';
+    let img;
+    nodeFetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+        img = data.img;
+        res.render('index', {img:img});
+    })
+    .catch(err => {
+        console.log(err);
+        res.render('index');
+    });
+});
